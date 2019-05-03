97380cdfe0c23698c482079b6925c0b19c057e7d
Goonie19 <strawhatgoonie@gmail.com>
2019-05-03 18:13:08 +0200

    Mas avanzado si cabe wow

--- worker.py
+++ worker.py
@@ -5,9 +5,23 @@ import os
 import sys
 import tweepy
 import time
+import paginas_drive.classy as gs
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import 
+
+proyecto_sd = gs.drive() # instacia para abstraer el drive, necesaria para comunicarse con drive
+
+hoja = proyecto_sd.nueva_hoja("Heroes") # creamos la hoja donde iremos actualizando las páginas
+proyecto_sd.nueva_pagina("Heroes","Marvel") #completar
+proyecto_sd.nueva_pagina("Heroes","dc") #completar
 
 con = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = con.channel()
+
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("client_id.json")
+drive = GoogleDrive(gauth)
 
 marvel = []
 dc = []
@@ -46,6 +60,30 @@ def contarTweets(consulta, array):
 	marvel[consulta] = cont
 
 
+#Arreglar para que coja el top 5 con lista.pop(indice)
+def topSuperHeroesMarvel():
+	array = []
+	i = 0
+	c = 0
+	max = 0
+	max2 = 9999999
+	cadena = ""
+	while c < 4:
+		while i < 12:
+			if (marvel[heroes[i]] > max):
+				max = marvel[heroes[i]]
+				cadena = heroes[i]
+				i += 1
+			else
+				i += 1
+		i = 0
+		c += 1
+		max = 0
+		array.append(cadena)
+
+
+
+
 def llamadaMarvel():
 	i = 0
 	for n in heroes:
@@ -60,13 +98,20 @@ def llamadaDC():
 			contarTweets(heroes[i], dc)
 		i += 1
 
-def topSuperHeroes():
+def topSuperHeroesDC():
 
 
 def callBack(ch, method, properties, body):
-	if(body == 'Marvel' or '"top_heroes Marvel'):
+	if(body == 'Marvel' or body == '@top_heroes Marvel'):
 		print(" Recogiendo información sobre la popularidad de los heroes de Marvel")
 		llamadaMarvel()
+		top = topSuperHeroes()
+		cadena = "Este es el top de popularidad de los heroes de Marvel.\nTop 1: " + top[0] + "\nTop 2: " + top[1] + 
+		"\nTop 3: " + top[2] + "\nTop 4: " + top[3] + "\nTop 5:" + top[4]
+		#añadir creacion de fichero y subida a drive
+	else if(body == 'DC' or body == '@top_heroes DC'):
+		print(" Recogiendo información sobre la popularidad de los heroes de DC")
+		llamadaDC()
 		top = topSuperHeroes()
 		cadena = "Este es el top de popularidad de los heroes de Marvel.\nTop 1: " + top[0] + "\nTop 2: " + top[1] + 
 		"\nTop 3: " + top[2] + "\nTop 4: " + top[3] + "\nTop 5:" + top[4]
